741f2e78e9180e4fbd7b357943bd139265a10b0c
devstorm2576916 <ruiz.salvador951011@gmail.com>
2025-04-22 01:21:28 +0530

    chhotu fix

--- api/database/mongoDb.js
+++ api/database/mongoDb.js
@@ -3,12 +3,10 @@ import mongoose from 'mongoose';
 // Get MongoDB URI from environment variables
 
 // MongoDB connection function
-const connectDB = async (uri) => {
+const connectDB = async () => {
     try {
-    //   console.log(process.env.MONGODB_URI);
-    // const conn = await mongoose.connect(process.env.MONGODB_URI);
-    const dbURI = uri || "mongodb+srv://divyansh:KrHRg7mgeh7tgNiU@cluster0.qhaz53w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    const conn = await mongoose.connect(dbURI);
+      console.log(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     // mongoose.connection.
     return conn;
